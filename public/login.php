e40554bb7c57e981c97cd81f2793c03798d8ab72
NgGMinh <95074814+NgGMinh@users.noreply.github.com>
2022-04-27 16:31:50 +0700

    Update login.php

--- public/login.php
+++ public/login.php
@@ -11,11 +11,19 @@ $Datas = $Data->selectKhachHang();
 if (isset($_POST['login'])) {
     $email      = $_POST['email'];
     $matkhau    = $_POST['matkhau'];
-}
-foreach ($Datas as $Data) {
-    if ($Data->Email == $email && $Data->MatKhau == $matkhau) {
-        $_SESSION['user'] = $Data->HoTenKH;
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
-        exit;
+    $url = $_SERVER['HTTP_REFERER'];
+    $checkLogin=0;
+    
+    foreach ($Datas as $Data) {
+        if ($Data->Email == $email && $Data->MatKhau == $matkhau) {
+            $_SESSION['user'] = $Data->HoTenKH;
+            echo '<script>if(confirm("Đăng nhập thành công"))';
+            echo "{document.location = '$url'};</script>";
+            $checkLogin = 1;
+        }
+    }
+    if($checkLogin == 0){
+        echo '<script>alert("Sai tài khoản hoặc mật khẩu! Đăng nhập thất bại!")</script>';
+        echo "<script>document.location = '$url'</script>";
     }
 }
